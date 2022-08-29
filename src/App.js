362eeacdad0187c3da9a55cc9078ce5e1ad1e3a8
Kevin Hu <khu@reggora.com>
2022-08-29 18:21:26 -0400

    added buttons with randomized text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
 import React, { useState } from "react";
+import ChoiceDisplay from "./components/ChoiceDisplay";
+import ImageDisplay from "./components/ImageDisplay";
+
+const pearl = [
+  "Records",
+  "Restaurant",
+  "Screen",
+  "Secret",
+  "Shops",
+  "Spawn",
+  "Top",
+  "Tower",
+  "Tunnel",
+  "Water"
+];
+
+const pearlImages = [
+  {
+    imageName: "pearl_secret.png",
+    callout: "Shops"
+  },
+  {
+    imageName: "pearl_secret.png",
+    callout: "Shops"
+  },
+  {
+    imageName: "pearl_secret.png",
+    callout: "Shops"
+  }
+];
+
+const seenPool = []; // pool of seen callouts
+
+const choicePool = pearl.sort(() => Math.random() - Math.random()).slice(0, 3); // todo: make this option for number of choices
 
 export default () => {
-  return <div> yeehaw </div>;
+  //const [map, setMap] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState(pearlImages[0]);
+  const [choices, setChoices] = useState([]);
+  const [seen, setSeen] = useState([]);
+  //const [correctScore, setCorrectScore] = useState([]);
+  //const [wrongScore, setWrongScore] = useState;
+  return (
+    <div>
+      <ImageDisplay imageName={pearlImages[0].imageName}></ImageDisplay>
+      <ChoiceDisplay choices={choicePool}></ChoiceDisplay>
+    </div>
+  );
 };
