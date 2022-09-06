2e9c5d5c669e456aa2d0576c49a46b4650f29180
Kevin Hu <khu@reggora.com>
2022-09-06 18:15:44 -0400

    can click buttons and update score

--- src/App.js
+++ src/App.js
@@ -1,50 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ChoiceDisplay from "./components/ChoiceDisplay";
 import ImageDisplay from "./components/ImageDisplay";
-
-const pearl = [
-  "Records",
-  "Restaurant",
-  "Screen",
-  "Secret",
-  "Shops",
-  "Spawn",
-  "Top",
-  "Tower",
-  "Tunnel",
-  "Water"
-];
 
 const pearlImages = [
   {
     imageName: "pearl_secret.png",
-    callout: "Shops"
+    callout: "Secret"
+  },
+  {
+    imageName: "chicken1.png",
+    callout: "Records"
+  },
+  {
+    imageName: "chicken2.png",
+    callout: "Restaurant"
   },
   {
     imageName: "pearl_secret.png",
     callout: "Shops"
   },
   {
-    imageName: "pearl_secret.png",
-    callout: "Shops"
+    imageName: "chicken1.png",
+    callout: "Spawn"
+  },
+  {
+    imageName: "chicken2.png",
+    callout: "Water"
   }
 ];
 
-const seenPool = []; // pool of seen callouts
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
 
-const choicePool = pearl.sort(() => Math.random() - Math.random()).slice(0, 3); // todo: make this option for number of choices
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex]
+    ];
+  }
+
+  return array;
+}
 
 export default () => {
-  //const [map, setMap] = useState([]);
-  const [correctAnswer, setCorrectAnswer] = useState(pearlImages[0]);
+  const [map, setMap] = useState("Pearl");
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [image, setImage] = useState("");
   const [choices, setChoices] = useState([]);
   const [seen, setSeen] = useState([]);
-  //const [correctScore, setCorrectScore] = useState([]);
-  //const [wrongScore, setWrongScore] = useState;
+  const [correctScore, setCorrectScore] = useState(0);
+  const [incorrectScore, setIncorrectScore] = useState(0);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * pearlImages.length); //should change this to random elem in total arr then pop
+    const answer = pearlImages[randomIndex].callout; //should change this to random elem in total arr then pop
+    setCorrectAnswer(answer);
+    setImage(pearlImages[randomIndex].imageName);
+    var options = pearlImages
+      .map((loc) => loc.callout)
+      .filter((choice) => choice !== answer) // make sure no duplicate answer
+      //.filter((x) => !seen.includes(x))
+      .sort(() => Math.random() - Math.random())
+      .slice(0, 3); // todo: make this option for number of choices
+
+    setChoices(shuffle([...options, answer]));
+  }, [correctScore, incorrectScore]);
+
+  const onChoiceClicked = (choice) => {
+    if (correctAnswer === choice) {
+      setCorrectScore(correctScore + 1);
+    } else {
+      setIncorrectScore(incorrectScore + 1);
+    }
+  };
+
   return (
     <div>
-      <ImageDisplay imageName={pearlImages[0].imageName}></ImageDisplay>
-      <ChoiceDisplay choices={choicePool}></ChoiceDisplay>
+      <div>
+        Score: {correctScore} - {incorrectScore}
+      </div>
+      <div>Correct Answer: {correctAnswer}</div>
+      <ImageDisplay imageName={image}></ImageDisplay>
+      <ChoiceDisplay
+        choices={choices}
+        onChoiceClicked={onChoiceClicked}
+      ></ChoiceDisplay>
     </div>
   );
 };
