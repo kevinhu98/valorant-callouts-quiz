e2cbadbde27c65d5484a7999b5a2c5283a6f90ea
kevinhu98 <kh2547@nyu.edu>
2022-09-15 00:50:05 -0400

    added some css to fix spacing issues

--- src/components/ChoiceDisplay.js
+++ src/components/ChoiceDisplay.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./ChoiceDisplay.scss";
 
 const ChoiceDisplay = ({
   choices,
--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,12 +1,10 @@
 import React from "react";
-import "./ImageDisplay.css";
+import "./ImageDisplay.scss";
 
 const ImageDisplay = ({ imageName }) => {
   return (
-    <div className="ui container">
-      <div className="image-container">
-        <img src={`/images/${imageName}.png`}></img>
-      </div>
+    <div className="image-container">
+      <img src={`/images/${imageName}.png`}></img>
     </div>
   );
 };
